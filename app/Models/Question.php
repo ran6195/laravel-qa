b7d7ebead1436eda05104b4e66289a3aba0e46e2
Corsanello Francesco <37504667@telecomitalia.it>
2020-09-25 12:21:21 +0200

    Primo commit

---
+++ app/Models/Question.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 'title' , 'body' ];
+
+    public function user() {
+        return $this->belongsTo( User::class );
+    }
+
+    public function setTitleattribute( $value ) {
+        $this->attributes[ 'title' ] = $value;
+        $this->attributes[ 'slug' ] = Str::slug( $value );
+    }
+
+}
